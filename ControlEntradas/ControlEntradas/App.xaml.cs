2de019621a9ed46e542aaeb6827361ccda265133
Raul Avila Catala <cl2raul66_dev_gt@hotmail.com>
2023-12-13 04:53:14 -0600

    Agragado de Control de entradas

---
+++ ControlEntradas/ControlEntradas/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ControlEntradas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
